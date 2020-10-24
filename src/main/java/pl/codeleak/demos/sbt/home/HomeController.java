b238e8c4b1db387a7b7f6f646f6e6491ca870af8
eddyko <eddyko@eddyko-PC>
2020-10-24 13:58:55 -0400

    v2

--- src/main/java/pl/codeleak/demos/sbt/home/HomeController.java
+++ src/main/java/pl/codeleak/demos/sbt/home/HomeController.java
@@ -34,101 +34,41 @@ class HomeController {
     String account_1(Model model) {
         return "account_1";
     }
+    @GetMapping("/accountmsg")
+    String accountmsg(Model model) {
+        return "accountmsg";
+    }
+    @GetMapping("/accountst")
+    String accountst(Model model) {
+         return "accountst";
+    }
+    @GetMapping("/accountst_1")
+    String accountst_1(Model model) {
+        return "accountst_1";
+    }
     @GetMapping("/accountstatus")
     String accountstatus(Model model) {
         return "accountstatus";
     }
     @GetMapping("/accountstatus_1")
     String accountstatus_1(Model model) {
-         return "accountstatus_1";
+        return "accountstatus_1";
     }
-    @GetMapping("/monanalyize")
-    String monanalyize(Model model) {
-        return "monanalyize";
+    @GetMapping("/accountsttr")
+    String accountsttr(Model model) {
+        return "accountsttr";
     }
-    @GetMapping("/monmonitor")
-    String monmonitor(Model model) {
-        return "monmonitor";
+    @GetMapping("/accountsttr_1")
+    String accountsttr_1(Model model) {
+         return "accountsttr_1";
     }
-    @GetMapping("/monmonitor_1")
-    String monmonitor_1(Model model) {
-        return "monmonitor_1";
+    @GetMapping("/accounttran")
+    String accounttran(Model model) {
+        return "accounttran";
     }
-    @GetMapping("/monmonitor_2")
-    String monmonitor_2(Model model) {
-        return "monmonitor_2";
-    }
-    @GetMapping("/monmonitortc")
-    String monmonitortc(Model model) {
-         return "monmonitortc";
-    }
-    @GetMapping("/monserv")
-    String monserv(Model model) {
-        return "monserv";
-    }
-    @GetMapping("/monserv_1")
-    String monserv_1(Model model) {
-         return "monserv_1";
-    }
-    @GetMapping("/monservfeat")
-    String monservfeat(Model model) {
-         return "monservfeat";
-    }
-    @GetMapping("/monservfeat_1")
-    String monservfeat_1(Model model) {
-        return "monservfeat_1";
-    }
-    @GetMapping("/monservfeattest")
-    String monservfeattest(Model model) {
-        return "monservfeattest";
-    }
-    @GetMapping("/monservfeattestres")
-    String monservfeattestres(Model model) {
-         return "monservfeattestres";
-    }
-    @GetMapping("/monservfeattestres_1")
-    String monservfeattestres_1(Model model) {
-         return "monservfeattestres_1";
-    }
-    @GetMapping("/reganalyize")
-    String reganalyize(Model model) {
-         return "reganalyize";
-    }
-    @GetMapping("/regmonitor")
-    String regmonitor(Model model) {
-         return "regmonitor";
-    }
-    @GetMapping("/regmonitor_1")
-    String regmonitor_1(Model model) {
-         return "regmonitor_1";
-    }
-    @GetMapping("/regmonitor_2")
-    String regmonitor_2(Model model) {
-         return "regmonitor_2";
-    }
-    @GetMapping("/regmonitortc")
-    String regmonitortc(Model model) {
-         return "regmonitortc";
-    }
-    @GetMapping("/splunkanalyize")
-    String splunkanalyize(Model model) {
-         return "splunkanalyize";
-    }
-    @GetMapping("/splunkserv")
-    String splunkserv(Model model) {
-         return "splunkserv";
-    }
-    @GetMapping("/splunkserv_1")
-    String splunkserv_1(Model model) {
-         return "splunkserv_1";
-    }
-    @GetMapping("/splunkservfeat")
-    String splunkservfeat(Model model) {
-         return "splunkservfeat";
-    }
-    @GetMapping("/splunkservfeat_1")
-    String splunkservfeat_1(Model model) {
-         return "splunkservfeat_1";
+    @GetMapping("/accounttran_1")
+    String accounttran_1(Model model) {
+         return "accounttran_1";
     }
 //////////////////////////////////////
     @GetMapping("properties")
